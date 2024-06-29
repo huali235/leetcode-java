489efebe35f64069fa6b67ec4c42171ba04c5a38
Hussain Ali <huali235@gmail.com>
2024-06-29 12:44:48 -0400

    isPalindrome solved

---
+++ src/main/java/PalindromeLinkedList.java
@@ -0,0 +1,44 @@
+/*
+Given the head of a singly linked list, return true if it is a
+palindrome or false otherwise.
+
+Example 1:
+Input: head = [1,2,2,1]
+Output: true
+ */
+
+public class PalindromeLinkedList {
+    public boolean isPalindrome(ListNode head){
+        if(head == null || head.next == null){
+            return true;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode last = null;
+        ListNode current = slow;
+
+        while(current != null){
+            ListNode newNode = current.next;
+            current.next = last;
+            last = current;
+            current = newNode;
+        }
+        ListNode secondHalf = last;
+
+        ListNode firstHalf = head;
+        while(secondHalf != null){
+            if(firstHalf.val != secondHalf.val){
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+        return true;
+    }
+}
